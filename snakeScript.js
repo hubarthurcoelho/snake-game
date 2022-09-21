c7ef96caa31b7ff6e762e42a2bfc031b255de38c
Arthur Coelho <arthurcoelho7885@gmail.com>
2022-09-21 16:42:34 -0300

    basic functions working

--- snakeScript.js
+++ snakeScript.js
@@ -3,7 +3,7 @@ const context = canvas.getContext('2d');
 const body = document.body;
 canvas.style.boxShadow = 'black 20px 10px 50px';
 
-class SnakeBits {
+class SnakeBit {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -21,13 +21,17 @@ let appleX = 5;
 let appleY = 5;
 let xSpeed = 0;
 let ySpeed = 0;
+let score = 0;
 
 function gameMotion() {
-  clearScreen();
   updateSnakePosition();
+  const result = isGameOver();
+  if (result) return;
+  clearScreen();
   checkAppleColision();
   drawApple();
   drawSnake();
+  drawScore();
   setTimeout(gameMotion, 1000/speed);
 };
 
@@ -37,6 +41,17 @@ function clearScreen() {
 };
 
 function drawSnake() {
+  
+  context.fillStyle = 'green';
+  for (let i = 0; i < snakeBits.length; i++) {
+    let bit = snakeBits[i];
+    context.fillRect(bit.x * tileCount, bit.y * tileCount, tileSize, tileSize);
+  };
+  snakeBits.push(new SnakeBit(snakeX, snakeY));
+  if (snakeBits.length > snakeLength) {
+    snakeBits.shift();
+  };
+
   context.fillStyle = 'orange';
   context.fillRect(snakeX * tileCount, snakeY * tileCount, tileSize, tileSize);
 };
@@ -56,6 +71,7 @@ function checkAppleColision() {
     appleX = Math.floor(Math.random() * tileCount);
     appleY = Math.floor(Math.random() * tileCount);
     snakeLength += 1;
+    score += 10;
   };
 };
 
@@ -89,6 +105,38 @@ function keyDown(event) {
     xSpeed = 0;
   };
 };
+
+function drawScore() {
+  context.fillStyle = 'white';
+  context.font = '20px Verdana';
+  context.fillText(`Score: ${score}`, 10, 25);
+};
+
+function isGameOver() {
+  let gameOver = false;
+
+  if (xSpeed === 0 && ySpeed === 0) return false;
+
+  if (snakeX < 0) gameOver = true;
+  if (snakeX === tileCount) gameOver = true;
+  if (snakeY < 0) gameOver = true;
+  if (snakeY === tileCount) gameOver = true;
+
+  for (let i = 0; i < snakeBits.length; i+= 1) {
+    let part = snakeBits[i];
+    if (part.x === snakeX && part.y === snakeY) {
+      gameOver = true;
+      break;
+    };
+  };
+
+  if (gameOver) {
+    context.font = '40px Verdana';
+    context.fillText('Game Over!', canvas.width / 5.6, canvas.height / 2);
+  };
+
+  return gameOver;
+}
 
 body.addEventListener('keydown', keyDown);
 
